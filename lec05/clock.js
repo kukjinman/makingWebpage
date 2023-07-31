d935be2d1862b2baaa18c104ce7a398ef3291504
kukjin <thater114@gmail.com>
2023-07-31 21:05:04 +0900

    EP05-2 commit

--- lec05/clock.js
+++ lec05/clock.js
@@ -1,5 +1,11 @@
 
-const curtime = new Date()
+function updateClock() {
+    console.log("updateClock is called")
 
-const timeString = String(curtime.getHours()).padStart(2, "0") + ":" + String(curtime.getMinutes()).padStart(2, "0") + ":" + String(curtime.getSeconds()).padStart(2, "0")
-alert(timeString)
+    const curtime = new Date()
+    const timeString = String(curtime.getHours()).padStart(2, "0") + ":" + String(curtime.getMinutes()).padStart(2, "0") + ":" + String(curtime.getSeconds()).padStart(2, "0")
+    // alert(timeString)
+    console.log(timeString)
+}
+
+setInterval(updateClock, 1000)
