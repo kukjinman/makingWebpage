3dad68923dfe9cf086e32afeea85ab49fb4f2cdb
kukjin <thater114@gmail.com>
2023-09-16 14:32:19 +0900

    EP07-2 commit

--- lec07/js/calendar.js
+++ lec07/js/calendar.js
@@ -2,7 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        selectable: true,
+
+        dateClick: function (info) {
+            console.log("Clicked event occurs : date = " + info.dateStr);
+        }
+
     });
     calendar.render();
 });
