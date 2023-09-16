9d4bc56b5d177df963e69620df500407fe687e34
kukjin <thater114@gmail.com>
2023-09-16 21:29:11 +0900

    ep07-3

--- lec07/js/calendar.js
+++ lec07/js/calendar.js
@@ -1,14 +1,23 @@
 
+var calendar;
+
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+    calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         selectable: true,
+        events: [],
 
         dateClick: function (info) {
             console.log("Clicked event occurs : date = " + info.dateStr);
+
+            addEventToCalendar({ start: info.dateStr });
         }
 
     });
     calendar.render();
 });
+
+function addEventToCalendar(event) {
+    calendar.addEvent(event);
+}
