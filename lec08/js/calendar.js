ae39a509854c68f836d78b0630df681616e11ce4
kukjin <thater114@gmail.com>
2023-09-30 16:13:35 +0900

    EP08-0

---
+++ lec08/js/calendar.js
@@ -0,0 +1,31 @@
+
+var calendar;
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        selectable: true,
+        events: [{ id: "2023-09-14", start: "2023-09-14" }, { id: "2023-09-15", start: "2023-09-15" }],
+
+        dateClick: function (info) {
+            console.log("Clicked event occurs : date = " + info.dateStr);
+
+            // addEventToCalendar({ start: info.dateStr });
+
+            removeEventFromCalendar(info.dateStr);
+
+        }
+
+    });
+    calendar.render();
+});
+
+function addEventToCalendar(event) {
+    calendar.addEvent(event);
+}
+
+function removeEventFromCalendar(id) {
+    var calendarEvent = calendar.getEventById("2023-09-14");
+    calendarEvent.remove();
+}
