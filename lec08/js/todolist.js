423778d82135b03174233f77b1cce1e5a9909d1e
kukjin <thater114@gmail.com>
2023-10-11 22:35:54 +0900

    EP08-2

--- lec08/js/todolist.js
+++ lec08/js/todolist.js
@@ -1,5 +1,6 @@
 const todoform = document.querySelector("#todoform");
 const todoInput = document.querySelector("#todoform input");
+const todoList_ul = document.querySelector("#todolist");
 
 todoform.addEventListener("submit", handleToDoSummit);
 
@@ -10,4 +11,14 @@ function handleToDoSummit(parm) {
     console.log("todoInput value : " + curTodo);
 
     todoInput.value = "";
+
+    const todo_cur_li = document.createElement("li");
+    const todo_cur_span = document.createElement("span");
+
+    todo_cur_span.innerText = curTodo;
+
+    todo_cur_li.appendChild(todo_cur_span);
+
+    todoList_ul.appendChild(todo_cur_li);
+
 }
