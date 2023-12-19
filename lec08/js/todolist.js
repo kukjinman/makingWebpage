5b338c5e46c485b8d254f5cac77a02bdc4d2ea55
kukjin <thater114@gmail.com>
2023-12-19 23:45:20 +0900

    EP08-10

--- lec08/js/todolist.js
+++ lec08/js/todolist.js
@@ -6,8 +6,13 @@ const curDate_p = document.querySelector("#cur_date");
 todoform.addEventListener("submit", handleToDoSummit);
 
 let DBLists = [];
+let CurrentDate;
 const DBLIST_KEY = "DBLISTS"
 
+function TodoList(date) {
+    this.date = date;
+    this.todos = [];
+}
 
 function handleToDoSummit(parm) {
     console.log("handleToDoSummit is called");
@@ -26,6 +31,8 @@ function handleToDoSummit(parm) {
 
     todoList_ul.appendChild(todo_cur_li);
 
+    addNewTodo(CurrentDate, curTodo);
+
     // save localstorage code
     saveDBListInLocalStorage();
 }
@@ -33,6 +40,15 @@ function handleToDoSummit(parm) {
 function setCurrentDate(date) {
     console.log("setCurrentDate - " + date);
     curDate_p.textContent = date + " 일정";
+    CurrentDate = date;
+}
+
+function addNewTodo(date, newTodo) {
+    console.log("addNewTodo is called");
+    curTodoList = new TodoList(date);
+    curTodoList.todos.push(newTodo);
+
+    DBLists.push(curTodoList);
 }
 
 function saveDBListInLocalStorage() {
